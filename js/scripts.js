08288e68e7525590fdbfb4347c68593a878c300e
beauwilliams <beau.williams@icloud.com>
2021-11-04 16:02:55 +1100

    comingsoon

--- js/scripts.js
+++ js/scripts.js
@@ -60,14 +60,4 @@ $(function () {
         }, 1000);
     });
 
-    /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
-    });
 });
