5a79de988bed9759390617e2f67887f1bccae97e
beauwilliams <beau.williams@icloud.com>
2021-11-07 12:32:48 +1100

    adding note about heroku deployement

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(express.static('public'));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 // Port website will run on
+// FOR HEROKU
+// https://stackoverflow.com/questions/15693192/heroku-node-js-error-web-process-failed-to-bind-to-port-within-60-seconds-of
 app.listen(process.env.PORT || 8000)
 
 // *** GET Routes - display pages ***
