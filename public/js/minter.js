69216a9fb27988f71c7fec1c3b8279e195354832
beauwilliams <beau.williams@icloud.com>
2021-11-13 10:38:54 +1100

    test

--- public/js/minter.js
+++ public/js/minter.js
@@ -51,7 +51,7 @@ async function loginWithMetaMask() {
     loginButton.innerText = 'Disconnect Wallet'
 
     if (correctNetwork()) {
-    userNetwork.innerText = "Successfuly connected to the BSC testnet.. test"
+    userNetwork.innerText = "Successfuly connected to the BSC testnet.. test.."
     }
     else {
         userNetwork.innerText = "You must be connected to the BSC testnet to use this app"
